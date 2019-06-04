7c9ebbe0c07a279bd0821569920b656579fc519e
k47ha <kgross@uos.de>
2019-06-05 00:41:50 +0200

    removed line that sphinx cpuld not parse

--- preprocessor.py
+++ preprocessor.py
@@ -9,25 +9,26 @@ from scipy.ndimage import label, find_objects
 
 
 def binarize_asparagus_img(img):
-    
     def blue_delta(img):
         """
         returns the delta between blue and the avg other channels 
         """
-        other = np.mean(img[:,:,0:2], axis=2)
-        return img[:,:,2]-other
-    
+        other = np.mean(img[:, :, 0:2], axis=2)
+        return img[:, :, 2]-other
+
     img_bw = np.sum(img, axis=2)
     img_bw = img_bw/np.max(img_bw)*255
     white = img_bw > 90
     blue = blue_delta(img) > 25
     return np.logical_and(white, np.invert(blue))
 
+
 def filter_mask_img(img):
-    img = binary_opening(img, structure=np.ones((21,21))) # 21,21
-    img = binary_dilation(img, np.ones((60,20))) #sometimes, it cuts the heads off, so mostly vertical dilation
+    img = binary_opening(img, structure=np.ones((21, 21)))  # 21,21
+    # sometimes, it cuts the heads off, so mostly vertical dilation
+    img = binary_dilation(img, np.ones((60, 20)))
     # unnecessary with new approach
-    #img = binary_closing(img, structure=np.ones((35,35))) # 45,45
+    # img = binary_closing(img, structure=np.ones((35,35))) # 45,45
     return img
 
 
@@ -35,7 +36,7 @@ def preprocessor(img_dir, target_dir, show=True, save=False, debug=True, time_co
     """
     Walks over a directory full of images, detects asparagus in those images, extracts them with minimal bounding box
     into an image of shape height x width and stores that image in target dir with a simple name.
-    
+
     args:
     img_dir         = the directory with raw images as string
     target_dir      = the target directory as string (where to store clean images)
@@ -49,8 +50,8 @@ def preprocessor(img_dir, target_dir, show=True, save=False, debug=True, time_co
     """
     starttime = time.time()
     ready = False
-    with open(os.path.join(img_dir,"progress.txt"), "r+") as progress, open(os.path.join(target_dir, "names.csv"), "a") as names :
-        
+    with open(os.path.join(img_dir, "progress.txt"), "r+") as progress, open(os.path.join(target_dir, "names.csv"), "a") as names:
+
         current = progress.read()
         if current == "":
             ready = True
@@ -61,24 +62,26 @@ def preprocessor(img_dir, target_dir, show=True, save=False, debug=True, time_co
             # over this, no idea where that comes from (maybe the truncate down below?)
             current = current.strip('\t\r\n\0')
             idx = int(idx) + 1
-        
+
         for subdir, dirs, files in os.walk(img_dir):
-            files = sorted([f for f in files if not f[0] == '.' and f[-4:] == '.bmp'])
+            files = sorted([f for f in files if not f[0]
+                            == '.' and f[-4:] == '.bmp'])
             for file in files:
                 # print(os.path.join(subdir, file))
                 if((not ready) and current == file):
                     ready = True
                     continue
                 if ready:
-                    
+
                     if(not time_constraint is None):
                         now = time.time()
-                        if( time_constraint * 60 - (now-starttime) < 30 ):
+                        if(time_constraint * 60 - (now-starttime) < 30):
                             sys.exit(0)
                     # gathering metadata
                     splits = file.split("-")
-                    date = splits[1] # like 190411
-                    batch_number, photo_number = splits[3].split("_") # like [874][F01.bmp]
+                    date = splits[1]  # like 190411
+                    batch_number, photo_number = splits[3].split(
+                        "_")  # like [874][F01.bmp]
                     photo_number = int(photo_number.split(".")[0][2:])
                     if(debug):
                         print("Date: {}".format(date))
@@ -86,17 +89,17 @@ def preprocessor(img_dir, target_dir, show=True, save=False, debug=True, time_co
                         print("Photo: {}".format(photo_number))
 
                     # defining boundaries (where to cut image)
-                    subimg_boundary = [(0,400),(300,700),(600,1000)]
+                    subimg_boundary = [(0, 400), (300, 700), (600, 1000)]
 
                     # reading in image
-                    img = plt.imread(os.path.join(subdir,file))
+                    img = plt.imread(os.path.join(subdir, file))
 
                     # cutting image
                     start, stop = subimg_boundary[photo_number]
-                    subimg = img[0:1300,start:stop,:]
+                    subimg = img[0:1300, start:stop, :]
 
                     if(debug):
-                        plt.figure(figsize=(20,20))
+                        plt.figure(figsize=(20, 20))
                         plt.imshow(subimg)
                         plt.show()
 
@@ -107,7 +110,7 @@ def preprocessor(img_dir, target_dir, show=True, save=False, debug=True, time_co
                     mask = filter_mask_img(binary)
 
                     if(debug):
-                        plt.figure(figsize=(20,20))
+                        plt.figure(figsize=(20, 20))
                         plt.imshow(mask)
                         plt.show()
 
@@ -115,10 +118,11 @@ def preprocessor(img_dir, target_dir, show=True, save=False, debug=True, time_co
                     labeled_mask, num_features = label(mask)
 
                     # turn our mask into a color mask
-                    cmask = np.stack([mask,mask,mask], axis=2)
+                    cmask = np.stack([mask, mask, mask], axis=2)
 
                     # mask the image
-                    masked = np.where(cmask, subimg, np.ones(subimg.shape, dtype=np.uint8))
+                    masked = np.where(cmask, subimg, np.ones(
+                        subimg.shape, dtype=np.uint8))
 
                     # find bounding box around every object, pieces = list of tuples, which are corners of bb:
                     # pieces[0] contains (x1,x2,None), (y1,y2,None) where x1y1 is the upper left coordinate and x2y2 the lower right
@@ -130,29 +134,33 @@ def preprocessor(img_dir, target_dir, show=True, save=False, debug=True, time_co
                         delta_h = hs.stop - hs.start
                         delta_w = ws.stop - ws.start
 
-                        if( delta_w < 20 or delta_h < 300): # if the segment is too small, it was probably a light reflex
+                        # if the segment is too small, it was probably a light reflex
+                        if(delta_w < 20 or delta_h < 300):
                             print("artifact discovered in {}".format(file))
                         else:
                             # patch is asparagus, extract to new image and add some padding
-                            new_img = masked[hs.start:hs.stop,ws.start:ws.stop,:]
+                            new_img = masked[hs.start:hs.stop,
+                                             ws.start:ws.stop, :]
                             lr = max_width - delta_w
                             ud = max_height - delta_h
                             new_img = np.pad(new_img, ((int(np.floor(ud/2)), int(np.ceil(ud/2))),
-                                                       (int(np.floor(lr/2)), int(np.ceil(lr/2))),
-                                                       (0,0)), "constant", constant_values=0)
+                                                       (int(np.floor(lr/2)),
+                                                        int(np.ceil(lr/2))),
+                                                       (0, 0)), "constant", constant_values=0)
                             if(show):
-                                plt.figure(figsize=(15,15))
+                                plt.figure(figsize=(15, 15))
                                 plt.imshow(new_img)
                                 plt.show()
                             if(save):
-                                plt.imsave(os.path.join(target_dir,str(idx)+"_"+str(photo_number)+".jpg"),new_img)
-                    
+                                plt.imsave(os.path.join(target_dir, str(
+                                    idx)+"_"+str(photo_number)+".jpg"), new_img)
+
                     if(photo_number == 2):
                         progress.truncate(0)
                         progress.write(file+"#"+str(idx))
                         names.write(file+","+str(idx)+"\n")
                         idx += 1
-                        
+
 
 if __name__ == "__main__":
 
@@ -160,18 +168,18 @@ if __name__ == "__main__":
     target_dir = "clean_images_demo/"
 
     try:
-        with open(os.path.join(img_dir,"progress.txt")) as file:
+        with open(os.path.join(img_dir, "progress.txt")) as file:
             pass
     except IOError:
-        file = open(os.path.join(img_dir,"progress.txt"), "w") 
-        file.close() 
-        
+        file = open(os.path.join(img_dir, "progress.txt"), "w")
+        file.close()
+
     try:
-        with open(os.path.join(target_dir,"names.csv")) as file:
+        with open(os.path.join(target_dir, "names.csv")) as file:
             pass
     except IOError:
-        file = open(os.path.join(target_dir,"names.csv"), "a") 
-        file.close() 
+        file = open(os.path.join(target_dir, "names.csv"), "a")
+        file.close()
 
     avg_width = 160
     avg_height = 1050
