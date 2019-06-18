0e3688320a3c6ce64847ce78d4d613972e1bfdff
k47ha <kgross@uos.de>
2019-06-19 00:45:32 +0200

    simple testing is working

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,14 +1,6 @@
 import pytest
-import math
-
-#good_style = __import__("code.good_style.py")
-
-from code.good_style import get_current_time
+import good_style
 
 
 def test_good_style_time_greater_zero():
-    assert(get_current_time() > 0)
-
-
-def test_square_of_2():
-    assert(math.sqrt(4) == 2)
+    assert(good_style.get_current_time() > 0)
