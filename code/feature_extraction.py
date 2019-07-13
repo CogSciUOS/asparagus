60f7bbda74260ece22059fe856ebb95725f50a66
Richard <rruppel@uni-osnabrueck.de>
2019-07-13 16:43:42 +0200

    transfered code from jupyter notebook to python script check_violett.ipynb -> feature_extraction

--- code/feature_extraction.py
+++ code/feature_extraction.py
@@ -1,5 +1,5 @@
-# created by Richard Ruppel at 2019/09/11
-# last changes from ******** at 2019/09/**
+# created by Richard Ruppel and Sophia Schulze-Weddige at 2019/09/11
+# last changes from RR and SSW at 2019/09/12
 
 # import area
 import doctest
@@ -12,32 +12,36 @@ from PIL import Image
 from scipy.ndimage.morphology import binary_hit_or_miss
 
 
-def preprocess_violett(path):
+def cut_background(img, background_max_hue, background_min_hue, background_brightness):
     """ Initiates masking in the hsv space.
 
-    Cuts out the area, where is not blue.
+    Cuts out background with specific hue values.
 
     Args:
-        path (string): path to Image
+        img (Image): Image as numpy array
+        background_max_hue (float): [0-255] 
+        background_min_hue (float): [0-255] 
+        background_brightness (float): [0-255] 
 
     Returns:
-        Image: processed Image 
+        Image: Image without background 
     """
-
+    
     # Open Image
-    raw = np.array(Image.open(path))
+    raw = img
     # remove alpha-channel (only if its RGBA)
     raw = raw[:,:,0:3]
     # transform to HSV color space
     hsv = matplotlib.colors.rgb_to_hsv(raw)
     
-    # Mask all blue hues
-    mask = np.logical_and(hsv[:,:,0]>0.4 , hsv[:,:,0]<0.8)
+    # Mask all blue hues (background)
+    mask = np.logical_and(hsv[:,:,0] > background_min_hue , hsv[:,:,0] < background_max_hue)
     # Mask out values that are not bright enough
-    mask = np.logical_or(hsv[:,:,2]<100,mask)
+    mask = np.logical_or(hsv[:,:,2]< background_brightness, mask)
     
     #Use binary hit and miss to remove potentially remaining isolated pixels:
     m = np.logical_not(mask)
+
     plt.figure()
     plt.imshow(m)
 
@@ -51,11 +55,14 @@ def preprocess_violett(path):
     
     plt.figure()
     plt.imshow(m)
+    plt.show()
 
     mask = np.logical_not(m)
     raw[:,:,0][mask] = 0
     raw[:,:,1][mask] = 0
     raw[:,:,2][mask] = 0
+
+
     return raw
 
 
@@ -64,61 +71,57 @@ def preprocess_violett(path):
 # TODO: 
 # - Return a meaningful value
 # - change input
-def get_violett(arg1):
+def get_violett(img, _max_set):
     """Checks for violett parts in the picture.
 
-    Extended description of function.
+    Calculates a value of how violett part in ???
 
     Args:
-        arg1 (int): Description of arg1
+        filePath (string): Path to image
 
     Returns:
-        bool: Description of return value
+        TODO: value: Description of return value
     """
 
-    img = preprocess_violett(arg1) 
-    # img = preprocess_violett(arg2) #np.array(Image.open(os.getcwd()))
-    # img = preprocess_violett(os.getcwd()+"/npurp3.png") #np.array(Image.open(os.getcwd()))
-    # img = preprocess_violett(np.array(Image.open(os.getcwd())))
-       
+
+    hue = matplotlib.colors.rgb_to_hsv(img)[:,:,0]
+    sat = matplotlib.colors.rgb_to_hsv(img)[:,:,1]
+
+    mask = ~np.logical_and(np.logical_and(img[:,:,0] == 0, img[:,:,1] == 0), img[:,:,2] == 0) # sobald eins true ist ist alles true
     
+    # TODO: discussible 
+    # set the pixel with low saturation to black
+    mask = np.logical_and(mask, sat > _max_set) 
+
+    #TODO:
+    # calcualte return value ( what makes sense? #Pixel above threshold? hue and or satuation values above threshold?)
+    # some normalized value [0 1]? 
+    # or boolean?
+    return_value = 0
+
+    return return_value
 
 
-
-    return True
-
-
-
-
-def func(arg1, arg2):
-    """Summary line.
-
-    Extended description of function.
-
-    Args:
-        arg1 (int): Description of arg1
-        arg2 (str): Description of arg2
-
-    Returns:
-        bool: Description of return value
-    """
-    return True
-
-
-
-
-
-
-
-
+# just for testing
 if __name__ == "__main__":
 
-    # TODO: Move to executing function, make sure file is there or open filePicker
-    # mask = preprocess_violett(os.getcwd() + "/npurp1.png")
+    # ugly but testing 
+    # @Katha how to write test functions? 
+    raw = np.array(Image.open(os.getcwd() + "/npurp1.png"))
+    plt.figure("main raw")
+    plt.imshow(raw)
 
+    # fix values:
+    background_max_hue = 0.8
+    background_min_hue = 0.4
+    background_brightness = 100.0
+    img = cut_background(raw, background_max_hue , background_min_hue, background_brightness)
+    
+    # fix values:
+    max_sat = 0.3
+    violett = 0
+    violett = get_violett(img, max_sat)
+
+    print("violett: ", violett)
     
     doctest.testmod()
-
-
-
-
