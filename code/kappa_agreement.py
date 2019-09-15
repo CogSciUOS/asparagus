54041ae71c93ddae7bc45f5b5074022da9c6a77d
k47ha <kgross@uos.de>
2019-09-15 16:16:30 +0200

    adding preliminary working version of kappa

--- code/kappa_agreement.py
+++ code/kappa_agreement.py
@@ -1,9 +1,17 @@
 from sklearn.metrics import cohen_kappa_score
+import sys
 
 import pandas as pd
+import numpy as np
 
 
-y_true = [2, 0, 2, 2, 0, 1]
-y_pred = [0, 0, 2, 2, 0, 2]
+annotations_1 = pd.read_csv("../annotations/annotator_1.csv", delimiter=";")
+annotations_2 = pd.read_csv("../annotations/annotator_2.csv", delimiter=";")
 
-print(cohen_kappa_score(y_true, y_pred))
+if np.any(annotations_1.isna()) or np.any(annotations_2.isna()):
+    print("There are missing values in the csv file. Fix it and come back here!")
+    sys.exit(1)
+
+for column in annotations_1.columns[1:]:
+    print("For the category", column, "the kappa is:", cohen_kappa_score(
+        annotations_1[column], annotations_2[column], labels=[1.0, 0.0]))
