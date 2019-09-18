e9749551264575c1d723c791c2fefa6063b82b5e
k47ha <kgross@uos.de>
2019-09-18 16:02:07 +0200

    very basic test for kappa to see if it works like this

---
+++ tests/test_kappa.py
@@ -0,0 +1,8 @@
+import kappa_agreement
+
+
+def test_kappa_equals_1_if_all_same():
+    # same file, so maximal agreement
+    kappa_dict = kappa_agreement.compute_agreement("../annotations/annotator_1.csv",
+                                                   "../annotations/annotator_1.csv")
+    assert(all(value == 1 for value in kappa_dict.values()))
