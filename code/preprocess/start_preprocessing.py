b39037dd940694e3f24e804635c1f6c5b9436206
Sophia <sschulzewedd@uos.de>
2019-10-31 14:23:46 +0100

    change outpath

--- code/preprocess/start_preprocessing.py
+++ code/preprocess/start_preprocessing.py
@@ -3,8 +3,8 @@ import os
 import csv
 
 if __name__ == "__main__":
-    path_to_valid_names = "/net/projects/scratch/summer/valid_until_31_January_2020/asparagus/Images/unlabled/valid_files.csv"
-    outpath = "/net/projects/scratch/summer/valid_until_31_January_2020/asparagus/Images/preprocessed_sep_11_2/background_removed/"
+    path_to_valid_names = "/net/projects/scratch/summer/valid_until_31_January_2020/asparagus/Images/unlabled/valid_files_31_10.csv"
+    outpath = "/net/projects/scratch/summer/valid_until_31_January_2020/asparagus/Images/preprocessed_oct_31/background_removed/"
     files_per_job = 10000
 	# get valid file names
     valid_triples = []
