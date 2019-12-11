d861d626b8d90614a3f90b5f9154d359d93bde8e
Richard <rruppel@uni-osnabrueck.de>
2019-12-11 12:37:36 +0100

    started impporting stuff - trouble with graphical response

---
+++ code/CreateDataSet/CreateDataset.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+import tensorflow_datasets as tfds
+
+
