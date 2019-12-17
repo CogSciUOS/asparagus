601e17cde22bd6a3aea835c512c1f79d82785628
Michael Gerstenberger <eler@gmx.net>
2019-12-17 17:36:15 +0100

    ...

--- code/variational_auto_encoder/variational_autoencoder.py
+++ code/variational_auto_encoder/variational_autoencoder.py
@@ -1,5 +1,6 @@
 from grid import *
 import argparse
+import subprocess
 
 if __name__ == "__main__":
         parser = argparse.ArgumentParser()
@@ -9,7 +10,9 @@ if __name__ == "__main__":
         environment = 'source /net/projects/scratch/winter/valid_until_31_July_2020/mgerstenberg/asparagus/code/grid_framework/test_environments/virtualenv/bin/activate'
         path = "/net/projects/scratch/winter/valid_until_31_July_2020/mgerstenberg/asparagus/code/variational_auto_encoder/variational_autoencoder_local.py"
         if args.mode == "local":
-            from variational_autoencoder_local import main
-            main()
+            p1 = subprocess.Popen([environment])
+            p1.wait()
+            #from variational_autoencoder_local import main
+            p2 = subprocess.Popen(["python " + path])
         else:
             submit_script(path,[],environment)
