55be86d3b9989796101ab4d38ebeccdebfec2bb4
mgerstenberg <mgerstenberg@light.cv.uni-osnabrueck.de>
2019-12-17 18:14:39 +0100

    updated runtime in submit script

--- code/preprocess/submit_script.py
+++ code/preprocess/submit_script.py
@@ -12,7 +12,7 @@ def submit_script(scriptpath, args, mem = "4G", cuda_cores = 0, jobname = "job")
     os.system('qsub -l mem='+ mem +' -l cuda_cores='+ str(cuda_cores)+" <<'toggle_here_document'\n"#use here document instead of loading a shell script from file
                 + "#!/bin/bash \n"
                 + "#$ -N " + jobname +"\n"
-                + "#$ -l h_rt=01:30:00" + "\n"
+                + "#$ -l h_rt=24:00:00" + "\n"
                 + "echo 'Start-time' \n"
                 + "date \n"
                 + "echo 'Host' \n"
