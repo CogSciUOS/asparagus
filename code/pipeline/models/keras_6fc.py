49a3fcb3d2822b125a53fa55787faf699f15281a
k47ha <kgross@uos.de>
2019-12-19 21:40:03 +0100

    changed to crossentropy

--- code/pipeline/models/keras_6fc.py
+++ code/pipeline/models/keras_6fc.py
@@ -6,8 +6,8 @@ def create_model(input_shape=(10, )):
     layer = inputs
     for i in range(6):
         layer = tf.keras.layers.Dense(20, activation='relu')(layer)
-    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(layer)
+    outputs = tf.keras.layers.Dense(2, activation='sigmoid')(layer)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
-    model.compile('adam', 'binary_crossentropy')
+    model.compile('adam', 'categorical_crossentropy')
     model.summary()
     return model
