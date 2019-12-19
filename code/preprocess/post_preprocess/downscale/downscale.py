f1f0ce12da5eb47a42607c85f1366def67fca267
mgerstenberg <mgerstenberg@light.cv.uni-osnabrueck.de>
2019-12-19 01:58:05 +0100

    moved

---
+++ code/preprocess/post_preprocess/downscale/downscale.py
@@ -0,0 +1,16 @@
+from grid import *
+import argparse
+from downscale_local import *
+
+if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument("mode",type=str, default="grid", choices=["grid","local"], help="Specify if the script shall run local for testing/debugging or in the grid")
+        args = parser.parse_args()
+
+        environment = 'source /net/projects/scratch/winter/valid_until_31_July_2020/mgerstenberg/asparagus/code/grid_framework/test_environments/virtualenv/bin/activate'
+        path = "/net/projects/scratch/winter/valid_until_31_July_2020/mgerstenberg/asparagus/code/preprocess/post_preprocess/downscale_local.py"
+        if args.mode == "local":
+            from downscale_local import main
+            main()
+        else:
+            submit_script(path,[],environment)
---
+++ code/preprocess/post_preprocess/downscale/downscale_local.py
@@ -0,0 +1,38 @@
+import os
+import pickle
+from PIL import Image
+import sys
+import numpy as np
+
+
+def main():
+    root = '/net/projects/scratch/summer/valid_until_31_January_2020/asparagus/preprocessed_images/with_background_rotated1/'
+    outroot = '/net/projects/scratch/summer/valid_until_31_January_2020/asparagus/preprocessed_images/without_background_downscaled/'
+
+    idx_to_files = {}
+    files = []
+
+    for gridjob_folder in os.listdir(root):
+        if os.path.isfile(gridjob_folder):
+            continue
+        for folder in os.listdir(os.path.join(root,gridjob_folder)):
+            if os.path.isfile(folder):
+                continue
+            full_folder = os.path.join(os.path.join(root,gridjob_folder),folder)
+            full_outfolder = os.path.join(os.path.join(outroot,gridjob_folder),folder)
+            os.makedirs(full_outfolder, exist_ok=True)
+
+            for i, file in enumerate(os.listdir(full_folder)):
+                if i % 100 == 0:
+                   print(i)
+                   sys.stdout.flush()
+                idx = int(file[:-6])
+                perspective = file[-5]
+                filepath = os.path.join(full_folder,file)
+                outpath = os.path.join(full_outfolder,file)
+
+                im = Image.open(filepath)
+                im = im.resize((36,134), resample=1)
+                im.save(outpath)
+if __name__ == "__main__":
+    main()
