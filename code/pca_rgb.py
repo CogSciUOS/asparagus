f863adf30c374c728099b591ae9e7967a190a238
marumse <mborn@uos.de>
2020-01-15 13:58:03 +0100

    Pca works fpr one rgb image

--- code/pca_rgb.py
+++ code/pca_rgb.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io,transform
 from skimage.transform import resize
+from numpy import linalg as LA
 
 
 #Using 6 sample images for the first try
@@ -9,31 +10,87 @@ from skimage.transform import resize
 imlist = (io.imread_collection('ex_images/*.png'))
 # this is our image size (1376, 1040)
 
+plt.imshow(imlist[1])
+plt.show()
+
 #mb = transform.resize(imlist, (1340, 364,3))
 # transform images to have all the same size
-#for i in range(len(imlist)):
+#for index in range(len(imlist)):
     # Using the skimage.transform function-- resize image (m x n x dim).
-#    m = transform.resize(imlist[i],(1340, 364,3))
-
-print(imlist[0].shape)
-print(type(imlist[0]))
-#print(m)
-#print(m) - ist alles nur mit 0 ist das ein problem?
-#print(imlist[0].shape())
+#    m = transform.resize(imlist[index],(1340, 364,3))
+#    plt.imshow(m)
+#    plt.show()
 
 
 #turn the image matrix of m x n x 3 to lists of rgb values i.e. (m*n) x 3.
 # initializing with zeros.
-# res = np.zeros(shape=(1,3))
-# #
-# for i in range(len(imlist)):
-#      #m = transform.resize(imlist[i],(1340, 364,3))
-#  	# Reshape the matrix to a list of rgb values.
-#      arr = imlist.reshape((1340*364),3)
-#      # concatenate the vectors for every image with the existing list.
-#      res = np.concatenate((res,arr),axis=0)
-#
-#  # delete initial zeros' row
-# res = np.delete(res, (0), axis=0)
-# # print list of vectors - 3 columns (rgb)
-# print(res)
+res = np.zeros(shape=(1,3))
+
+for i in range(len(imlist)):
+    # Using the skimage.transform function-- resize image (m x n x dim)
+    m = transform.resize(imlist[i],(1340, 364,3))
+ # Reshape the matrix to a list of rgb values.
+    arr = m.reshape((1340*364),3)
+ # concatenate the vectors for every image with the existing list.
+    res = np.concatenate((res,arr),axis=0)
+
+
+# delete initial zeros' row
+res = np.delete(res, (0), axis=0)
+
+#Subtract the mean. For PCA to work properly, you must subtract the mean from each of the dimensions
+# print list of vectors - 3 columns (rgb)
+m = res.mean(axis = 0)
+#print(m) [0.14694415 0.12558748 0.11998542]
+
+res = res - m
+
+R = np.cov(res, rowvar=False) #[[0.09184436 0.07866244 0.07482539]
+                              #[0.07866244 0.06747891 0.06427313]
+                              #[0.07482539 0.06427313 0.06147922]]
+
+evals, evecs = LA.eigh(R)
+idx = np.argsort(evals)[::-1]
+evecs = evecs[:,idx]
+
+# sort eigenvectors according to same index
+evals = evals[idx]
+
+# select the best 3 eigenvectors (3 is desired dimension
+# of rescaled data array)
+evecs = evecs[:, :3]
+
+# make a matrix with the three eigenvectors as its columns.
+evecs_mat = np.column_stack((evecs))
+
+# carry out the transformation on the data using eigenvectors
+# and return the re-scaled data, eigenvalues, and eigenvectors
+m = np.dot(evecs.T, res.T).T
+
+#img = imlist[0]/255.0
+
+def data_aug(img):
+    mu = 0
+    sigma = 0.1
+    feature_vec=np.matrix(evecs_mat)
+	# 3 x 1 scaled eigenvalue matrix
+    se = np.zeros((3, 1))
+    se[0][0] = np.random.normal(mu, sigma)*evals[0]
+    se[1][0] = np.random.normal(mu, sigma)*evals[1]
+    se[2][0] = np.random.normal(mu, sigma)*evals[2]
+    se = np.matrix(se)
+    val = feature_vec*se
+
+	# Parse through every pixel value.
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            # Parse through every dimension.fail
+            for k in range(img.shape[2]):
+                img[i,j,k] = float(img[i,j,k]) + float(val[k])
+
+# Calling function for first image.
+# Re-scaling from 0-255 to 0-1.
+img = imlist[1]/255.0
+data_aug(img)
+plt.imshow(img)
+plt.show()
