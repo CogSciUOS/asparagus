d438f408d5ae8459a558ae92895c849809cda9ab
MalinSpaniol <mspaniol@uos.de>
2020-01-17 12:33:31 +0100

    evecs_mat and evals are saved

--- code/pca_rgb.py
+++ code/pca_rgb.py
@@ -18,6 +18,7 @@ def data_aug(img):
     se[2][0] = np.random.normal(mu, sigma)*evals[2]
     se = np.matrix(se)
     val = feature_vec*se
+    #print(val.shape)
 
 	# Parse through every pixel value.
     for i in range(img.shape[0]):
@@ -26,29 +27,12 @@ def data_aug(img):
             for k in range(img.shape[2]):
                 img[i,j,k] = float(img[i,j,k]) + float(val[k])
 
-# function to save our calculated pc's in, not runnig yet - so far mor like a test
-def write_to_file(filename, input):
-    """takes filename and writes them into a csv_file
-
-    Arguments:
-        filename (str): how you want to name the output file with the agreement scores
-        kappa_dict (dict): dictionary with kappa score for each column
-    """
-    with open(filename, 'a') as csvfile:
-        writer = csv.writer(csvfile)
-
-        writer.writerow([str(input)])
-
 #Using 6 sample images for the first try
 
 #['0_a.png', '0_b.png','0_c.png','1_a.png', '1_b.png','1_c.png']
-#imlist = (io.imread_collection('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/Images/labled/kappa_images/1A_Anna/*.png'))
-imlist = (io.imread_collection('ex_images/*.png'))
+imlist = (io.imread_collection('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/Images/labled/kappa_images/1A_Anna/*.png'))
+#imlist = (io.imread_collection('ex_images/*.png'))
 # this is our image size (1376, 1040)
-
-# katha try out
-plt.imshow(imlist[1])
-plt.show()
 
 
 #turn the image matrix of m x n x 3 to lists of rgb values i.e. (m*n) x 3.
@@ -62,12 +46,11 @@ for i in range(len(imlist)):
     arr = m.reshape((1340*364),3)
  # concatenate the vectors for every image with the existing list.
     res = np.concatenate((res,arr),axis=0)
-    write_to_file("image.csv", i)
+    #write_to_file("image.csv", i)
 
 
 # delete initial zeros' row
 res = np.delete(res, (0), axis=0)
-print(res)
 
 #Subtract the mean
 # print list of vectors - 3 columns (rgb)
@@ -75,7 +58,6 @@ m = res.mean(axis = 0)
 #print(m) [0.14694415 0.12558748 0.11998542] - means of 3 columns
 
 res = res - m
-print(res)
 
 #Calculate the covariance matrix.
 #data is 3 dimensional,the cov matrix will be 3x3
@@ -83,7 +65,7 @@ R = np.cov(res, rowvar=False) #[[0.09184436 0.07866244 0.07482539]
                               #[0.07866244 0.06747891 0.06427313]
                               #[0.07482539 0.06427313 0.06147922]]
 
-evals, evecs = LA.eigh(R)
+evals, evecs = LA.eigh(R) #return eigenvalues and eigenvectors
 idx = np.argsort(evals)[::-1]
 evecs = evecs[:,idx]
 
@@ -94,6 +76,13 @@ evals = evals[idx]
 # of rescaled data array)
 evecs = evecs[:, :3]
 
+# evecs30 = evecs[:, :30]
+#
+# # save best 30 evecs as binary data, so that we can check the size of all eigenvectors
+# np.save('evecs_all.npy', evecs30)
+# # human readable format
+# np.savetxt('evecs_all.txt', evecs30)
+
 # make a matrix with the three eigenvectors as its columns.
 evecs_mat = np.column_stack((evecs))
 
@@ -101,15 +90,22 @@ evecs_mat = np.column_stack((evecs))
 # and return the re-scaled data, eigenvalues, and eigenvectors
 m = np.dot(evecs.T, res.T).T
 
-#img = imlist[0]/255.0
+
+# save evecs_mat as binary data
+np.save('evecs_mat.npy', evecs_mat)
+# human readable
+np.savetxt('evecs_mat.txt', evecs_mat)
+
+# save evals as binary data
+np.save('evals.npy', evals)
+# human readable
+np.savetxt('evals.txt', evals)
+
 
 
 # Calling function for first image.
 # Re-scaling from 0-255 to 0-1.
 img = imlist[1]/255.0
-plt.imshow(img)
-plt.show()
-#data_aug(img)
-
-
-write_to_file("test.csv", "test")
+# plt.imshow(img)
+# plt.show()
+data_aug(img)
