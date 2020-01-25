6093ef9eab634f88d9ced4398634cf0460a92fc5
Sophia <sschulzewedd@uos.de>
2020-01-25 13:58:15 +0100

    get array size

--- code/variational_auto_encoder/combine_npyy.py
+++ code/variational_auto_encoder/combine_npyy.py
@@ -6,9 +6,10 @@ import sys
 def combine(PATH):
     all_files = os.listdir(PATH)
     n = len(all_files)
-    first = np.load(PATH + all_files[0])
+    first = np.load(PATH + all_files[0])[::6,::6]
     dtype = first.dtype
-    data = np.empty((n, 1340, 364, 9), dtype=dtype)
+    l, w, d = fist.shape
+    data = np.empty((n, l, w, d), dtype=dtype)
     for i,file in enumerate(all_files):
         data[i,:,:,:] = np.load(PATH + file)[::6,::6]
         if i%500==0:
