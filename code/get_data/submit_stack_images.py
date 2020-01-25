34bb42449d11a24e7724cd4b96523ee7cacf6bee
Sophia <sschulzewedd@uos.de>
2020-01-25 15:52:04 +0100

    change path to changed destination

--- code/get_data/submit_stack_images.py
+++ code/get_data/submit_stack_images.py
@@ -2,7 +2,7 @@ from grid import*
 from stack_images import*
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/variational_auto_encoder/stack_images.py'
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/get_data/stack_images.py'
     path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/labeled_with_background'
     path_to_save = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/stacked_labeled_with_background/'
     args = [path_to_imgs, path_to_save]
