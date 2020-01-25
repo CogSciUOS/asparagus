7ef9c1e074fe503542a62d1bedb186f47eafafce
Sophia <sschulzewedd@uos.de>
2020-01-25 13:51:03 +0100

    add root to path

--- code/variational_auto_encoder/combine_npyy.py
+++ code/variational_auto_encoder/combine_npyy.py
@@ -6,11 +6,11 @@ import sys
 def combine(PATH):
     all_files = os.listdir(PATH)
     n = len(all_files)
-    first = np.load(all_files[0])
+    first = np.load(PATH + all_files[0])
     dtype = first.dtype
     data = np.empty((n, 1340, 364, 9), dtype=dtype)
     for i,file in enumerate(all_files):
-        data[i,:,:,:] = np.load(file)[::6,::6]
+        data[i,:,:,:] = np.load(PATH + file)[::6,::6]
         if i%500==0:
             print(i)
     path_out = PATH + "data.npy"
