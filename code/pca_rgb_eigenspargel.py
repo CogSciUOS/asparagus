74be49058af6a6ad3391c0ddc782c0a8df9c1a7d
MalinSpaniol <mspaniol@uos.de>
2020-01-28 08:57:16 +0100

    update of pca_rgb_eigenspargel

--- code/pca_rgb_eigenspargel.py
+++ code/pca_rgb_eigenspargel.py
@@ -5,20 +5,55 @@ from skimage.transform import resize
 from numpy import linalg as LA
 import csv
 import cv2
+import os
+
+def pca(data):
+    """
+    Perform principal component analysis.
+
+    Args:
+        data (ndarray): an array of shape (n,k),
+        meaning n entries with k dimensions
+
+    Returns: two arrays
+        pc (ndarray): array of shape (k,k) holding the principal components in its columns.
+        var (ndarray): k-vector holding the corresponding variances, in descending order.
+    """
+    data_centered = data - data.mean(axis=0)
+    cov_matrix = np.cov(data_centered, rowvar=False)
+    eigenvals, eigenvecs = np.linalg.eig(cov_matrix)
+    # sort eigenvalues and vectors
+    idx = np.argsort(eigenvals)[::-1]
+    eigenvecs = eigenvecs[:,idx]
+    eigenvals = eigenvals[idx]
+    # compute principal components
+    pc = eigenvecs.T @ data_centered
+    return eigenvecs, eigenvals, pc
 
 #Using 6 sample images for the first try
 
 #['0_a.png', '0_b.png','0_c.png','1_a.png', '1_b.png','1_c.png']
 #imlist = (io.imread_collection('/net/projects/scratch/summer/valid_until_31_January_2020/asparagus/Images/labled/kappa_images/2B/*.png'))
-imlist = (io.imread_collection('ex_images/*.png'))
+#imlist = (io.imread_collection('ex_images/*.png'))
+all_files = os.listdir('ex_images/')
+n = len(all_files)
+# load first image to get dimensionality and dtype dynamically
+first = np.array(plt.imread('ex_images/' + all_files[0]))[::6,::6] #change here to downscale differently
+dtype = first.dtype
+l, w, d = first.shape
+# make some space for the dataset
+data = np.empty((n, l, w, d), dtype=dtype)
+# load all files and save them in the corresponding position in the data array
+for i,file in enumerate(all_files):
+    data[i,:,:,:] = np.array(plt.imread('ex_images/' + file))[::6,::6] #change here to downscale differently
+        #before shape (40992) into shape (1463280)
 
-img_shape = imlist[0].shape
-# this is our image size (1376, 1040)
 
+imlist = data
 
 #turn the image matrix of m x n x 3 to lists of rgb values i.e. (m*n) x 3.
 # initializing with zeros.
-res = []
+res = np.zeros((len(imlist),40992))
 
 for i in range(len(imlist)):
     print(i)
@@ -33,29 +68,21 @@ for i in range(len(imlist)):
 
  # concatenate the vectors for every image with the existing list.
 
-    res.append(arr)
+    res[i,:] = arr
     print(res[0].shape)
-    print(len(res))
     # length x width x 3 : (1463280,)
     #write_to_file("image.csv", i)
 
 
 # PCA computation
-mean_face, eigen_vecs = cv2.PCACompute(res, mean = None)
+eigen_vecs, eigen_vals, eigenfaces = pca(res)
 
-# Generate eigenface from results
-eigen_faces = []
-for eigen_vec in eigen_vecs:
-    face = eigen_vec.reshape(img_shape)
-    eigen_faces.append(face)
-
-# Display eigenfaces
-plt.figure(figsize=(20,16))
+plt.figure(figsize=(12, 12))
 plt.gray()
-for i in range(20):
-    plt.subplot(5, 4, i+1)
+for i, eigenface in enumerate(eigenfaces):
+    plt.subplot(5, 4, i + 1)
     plt.axis('off')
-    plt.imshow(eigen_faces[i])
+    plt.imshow(eigenface.reshape(img_shape))
     plt.title('Eigenface {}'.format(i))
 plt.show()
 
