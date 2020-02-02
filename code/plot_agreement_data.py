6aae0f2496c18fed40990e9c8186f9edfb818ecd
k47ha <kgross@uos.de>
2020-02-02 23:56:12 +0100

    simple plots for agreement visualization

--- code/plot_agreement_data.py
+++ code/plot_agreement_data.py
@@ -6,7 +6,7 @@ import os
 agreement = pd.concat(map(pd.read_csv, glob.glob(os.path.join(
     '../annotations/evaluation_agreement_2', "agreement*.csv"))))
 
-print(agreement.head(30))
+# print(agreement.head(30))
 
 
 p = (ggplot(agreement, aes(x='evaluation_measure', y='score', group='annotators', color='feature'))
@@ -18,10 +18,32 @@ p = (ggplot(agreement, aes(x='evaluation_measure', y='score', group='annotators'
 # print(p)
 
 
-p2 = (ggplot(agreement, aes(x='evaluation_measure', y='score', group='annotators', color='feature'))
+p2 = (ggplot(agreement, aes(x='feature', y='score', group='feature', color='feature'))
       + geom_boxplot(aes(group='feature'))
       + facet_grid('.~evaluation_measure')
       + theme_bw())
 
 
-print(p2)
+# print(p2)
+
+
+p3 = (ggplot(agreement, aes(x='evaluation_measure', y='score', group='evaluation_measure', color='evaluation_measure'))
+      + geom_boxplot(aes(group='evaluation_measure'))
+      + facet_grid('.~feature')
+      + theme_bw()
+      )
+
+
+# print(p3)
+
+
+kappa = agreement[agreement["evaluation_measure"] == "kappa"]
+
+
+p4 = (ggplot(kappa, aes(x='feature', y='score', color='feature'))
+      + geom_boxplot(aes(group='feature'))
+      + theme_bw()
+      )
+
+
+print(p4)
