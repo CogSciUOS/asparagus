8e334d794390f2b179d317998b067f164d61e863
marumse <mborn@uos.de>
2020-02-03 19:02:12 +0100

    problem with shapes in function (line 149)

--- code/pca_colour_ac.py
+++ code/pca_colour_ac.py
@@ -50,7 +50,7 @@ for i in range(n_bands):
     MB_array = MB_img[:,:,i].flatten()  # covert 2d to 1d array
     MB_arrayStd = (MB_array - MB_array.mean())/MB_array.std()
     MB_matrix[:,i] = MB_arrayStd
-MB_matrix.shape;
+print(MB_matrix.shape)
 
 print(MB_matrix)
 
@@ -65,20 +65,97 @@ print("Eigenvalues:\n\n", EigVal,"\n")
 order = EigVal.argsort()[::-1]
 EigVal = EigVal[order]
 EigVec = EigVec[:,order]
-print(EigVec.shape)
+
+num_eigenvectors = 4
+eig_asparagus_used = EigVec[:num_eigenvectors] #Eigenvektoren, die wir benutzen
+
+#Eigen_used = EigVec[:num_eigenvectors]
+#print(Eigen_used.shape) #(130,4)
+#Eig_mean = EigVec.mean(axis=0)
+
+#asparagus_db = (EigVec - Eig_mean) @ Eigen_used.T
+#print(asparagus_db.shape)
 
 #Projecting data on Eigen vector directions resulting to Principal Components
-PC = np.matmul(MB_matrix,EigVec)   #cross product
+PC = np.matmul(MB_matrix,EigVec)   #cross product (1463280, 130)
+
+MB_matrix_mean = np.mean(MB_matrix, axis = 0)
+asparagus_space = (MB_matrix - MB_matrix_mean) @ eig_asparagus_used.T
+
 print(PC.shape)
 for i in range(10): #wir gucken uns die ersten 4 an, weil dort noch hohe eigenvalues zu sehen waren
-    test = PC[:,i].reshape(img_shape)
-    plt.imshow(test)
-    plt.show()
+     test = PC[:,i].reshape(img_shape)
+     plt.imshow(test)
+     plt.show()
+#PC = PC[:,4]
+#asparagus_vecs = MB_matrix.reshape((-1, np.prod(img_shape)))
+#print(asparagus_vecs.shape)
+#asparagus_mean = asparagus_vecs.mean()
+#print("asparagus_mean:\n", asparagus_mean)
+#asparagus_db = np.matmul((asparagus_vecs - asparagus_mean).T,Eigen_used)
+#print("asparagus_db: \n", asparagus_db.shape)
 
-x = range(10)#np.linspace(0,130, 1)
+
+#print(test.shape)#(1340, 364, 3)
+
+x = range(10)
+# np.linspace(0,130, 1)
 plt.plot(x,EigVal[:10])
 plt.show()
+
 #save eigenvalues
 #np.save('EigVal.npy', EigVal)
 # human readable
 #np.savetxt('evecs_mata.txt', evecs_mat)
+
+
+from scipy.spatial.distance import cdist
+#
+def recognize_face(face, eigenfaces, mean_face, face_db):
+    """
+    Recognize a face from a face database.
+    and return the index of the best matching database entry.
+
+    The FACE is first centered and projected into the eigeface
+    space provided by EIGENFACES. Then the best match is found
+    according to the euclidean distance in the eigenface space.
+
+    Args:
+        face (ndarray): Face to be recognised.
+        eigenfaces (ndarray): Array of eigenfaces.
+        mean_face (ndarray): Average face.
+        face_db (ndarray): Database of faces projectected into Eigenface space.
+
+    Returns:
+        index (uint): Position of the best matching face in face_db.
+    """
+    index = -1
+
+    # BEGIN SOLUTION
+    # center the face
+    face = np.reshape(img,newshape = (img_shape[0],img.shape[1]*img.shape[2]))
+    face_img = np.zeros((img_shape[0],img.shape[1]*img.shape[2]))  #(1376, 1040)
+    face_img = face
+    print(face_img.shape)
+
+    #face_array = face_img.flatten()
+#    print(face_array.shape)
+
+    centered = face - face_img
+    print(centered.shape)
+    print(eigenfaces.shape)
+
+    # and project it into the eigenface space
+    projected = np.matmul(eigenfaces,centered.T)
+
+    # Now compute the similarity to all known faces
+    # (comparison is performed in the eigenface space)
+    distances = cdist(face_db, projected[None, :])
+    index = distances.argmin()
+
+    # END SOLUTION
+
+    return index
+#
+
+recognize_face(img, PC, MB_matrix_mean, asparagus_space)
