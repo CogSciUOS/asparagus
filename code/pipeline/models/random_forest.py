251332f62bd9b42535849d9e9144f1f2df88935b
k47ha <kgross@uos.de>
2020-02-04 22:31:48 +0100

    simplified

--- code/pipeline/models/random_forest.py
+++ code/pipeline/models/random_forest.py
@@ -3,20 +3,19 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 class OneHotRandomForestClassifier(RandomForestClassifier):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.number_of_classes = None
 
-    def fit(self, x, y, *args, **kwargs):
+    def fit(self, x, y):
         self.number_of_classes = y.shape[1]
+        # convert one hot encoding to numeric class labels
         y = np.argmax(y, axis=1)
-        return super().fit(x, y, *args, **kwargs)
+        return super().fit(x, y)
 
-    def predict(self, x, *args, **kwargs):
-        y = super().predict(x, *args, **kwargs)
-        y_new = np.zeros((y.shape[0], self.number_of_classes), dtype=np.uint8)
-        ind = np.vstack([np.arange(y.shape[0]), y])
-        y_new[ind[0], ind[1]] = 1
+    def predict(self, x):
+        """convert back to one-hot encoding
+        """
+        y = super().predict(x)
+        y_new = np.zeros((y.shape[0], self.number_of_classes))
+        y_new[np.arange(y.shape[0]), y] = 1
         return y_new
 
 
