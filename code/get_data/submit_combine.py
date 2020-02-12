f45821597741a2fe81a23488cb8019df538d9d91
Sophia <sschulzewedd@uos.de>
2020-02-12 12:31:37 +0100

    call correct file in path

--- code/get_data/submit_combine.py
+++ code/get_data/submit_combine.py
@@ -2,7 +2,7 @@ from grid import*
 from stack_images import*
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/get_data/combine_npy.py'
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/get_data/combine_npy_files.py'
     path_to_npy = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/stacked_horizontal/0/'
     args = [path_to_npy]
    # env = 'source stack/bin/activate'
