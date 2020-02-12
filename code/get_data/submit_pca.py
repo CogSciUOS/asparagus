17ecbe2500989f6de066422c451b6dbcc4049daa
Sophia <sschulzewedd@uos.de>
2020-02-12 15:29:05 +0100

    Marens shit

--- code/get_data/submit_pca.py
+++ code/get_data/submit_pca.py
@@ -1,11 +1,11 @@
 from grid import*
-from pca import*
+
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/get_data/pca.py'
-    path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/pca_images_all_classes/'
-    num_eigenvecs = 10
-    args = [path_to_imgs, num_eigenvecs]
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/feature_ids.py'
+    path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/labeled_with_background/'
+    path_features = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/combined_new.csv'
+    args = [path_to_imgs, path_features]
    # env = 'source stack/bin/activate'
 
     submit_script(path, args)
