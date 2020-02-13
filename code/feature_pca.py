e0ebfc5c5813b16068b831049138f382c376065a
marumse <mborn@uos.de>
2020-02-13 10:23:26 +0100

    pca done for hollow

--- code/feature_pca.py
+++ code/feature_pca.py
@@ -13,7 +13,26 @@ img_shape = (1340, 364, 3)
 
 def calculate_PC(m_hollow):
 
-    np.load(m_hollowf)
+    #load data
+    m_hollow = np.load('Z:/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/m_hollow.npy')
+    print(m_hollow.shape) # das passt soweit
+    #show 2 images of the data
+    #plt.figure(figsize=(img_shape[0]/100,img_shape[1]/100)) #image size =  (1376, 1040)
+    plt.figure(figsize=(img_shape[0]/100,img_shape[1]/100))
+    testbild = m_hollow[1,:]
+    testbild_trans = np.reshape(testbild, newshape = (img_shape[0],img_shape[1],img_shape[2]))
+    plt.imshow(testbild_trans)# cmap = 'grey') das funzt nicht
+    plt.axis('off')
+    plt.show()
+
+    #plt.figure(figsize=(img_shape[0]/100,img_shape[1]/100)) #image size =  (1376, 1040)
+    plt.figure(figsize=(img_shape[0]/100,img_shape[1]/100))
+    testbild2 = m_hollow[202,:]
+    testbild2_trans = np.reshape(testbild2, newshape = (img_shape[0], img_shape[1], img_shape[2]))
+    plt.imshow(testbild2_trans)# vmin=0, vmax=255 cmap = 'grey')
+    plt.axis('off')
+    plt.show()
+
     #standardization of the matrix
     m_hollow_std = (m_hollow - m_hollow.mean())/m_hollow.std()
 
@@ -33,7 +52,7 @@ def calculate_PC(m_hollow):
 
     #calculate principle components
     PC = EigVec.T @ m_hollow_std
-    print(pc.shape)
+    print(PC.shape)
 
     #plot the first 10 eigenvalues to get an overview
     x = range(10)
@@ -42,76 +61,81 @@ def calculate_PC(m_hollow):
     plt.show()
 
     #look at the first 10 principle components
-    for i in range(10): #wir gucken uns die ersten 4 an, weil dort noch hohe eigenvalues zu sehen waren
-        test = PC[:,i].reshape(img_shape)
-        plt.imshow(test)
-        plt.show()
+    #for i in range(10): #wir gucken uns die ersten 4 an, weil dort noch hohe eigenvalues zu sehen waren
+    #    test = PC[i,:].reshape(img_shape)
+    #    plt.imshow(test)
+    #    plt.show()
 
     num_eigenvectors = 4 #lets see how many good ones we have
 
     eig_hollow_used = PC[:,:num_eigenvectors] #Eigenvektoren, die wir benutzen
-    print("Eig_hollow_used: \n", eig_asparagus_used.shape)
+    print("Eig_hollow_used: \n", eig_hollow_used.shape)
 
-    hollow_space = (m_hollow - m_hollow_std) @ eig_hollow_used
-    print("dim aspa_space: \n" , asparagus_space.shape)
+    hollow_space = (m_hollow - m_hollow_std) @ eig_hollow_used.T
+    print("dim aspa_space: \n" , hollow_space.shape)
+
+    np.save('hollow_space',hollow_space)
+    np.save('m_hollow_std', m_hollow_std)
+    np.save('eig_hollow_used', eig_hollow_used)
 
     return hollow_space, m_hollow_std, eig_hollow_used
 
+bla = []
+calculate_PC(bla)
 
 
-
-    def recognize_face(face, eigenfaces, mean_face, face_db):
-        """
-        Recognize a face from a face database.
-        and return the index of the best matching database entry.
-
-        The FACE is first centered and projected into the eigeface
-        space provided by EIGENFACES. Then the best match is found
-        according to the euclidean distance in the eigenface space.
-
-        Args:
-            face (ndarray): Face to be recognised.
-            eigenfaces (ndarray): Array of eigenfaces.
-            mean_face (ndarray): Average face.
-            face_db (ndarray): Database of faces projectected into Eigenface space.
-
-        Returns:
-            index (uint): Position of the best matching face in face_db.
-        """
-        index = -1
-
-        # BEGIN SOLUTION
-        # center the face
-        #face = np.reshape(img,newshape = (img_shape[0],img.shape[1]*img.shape[2]))
-        #face_img = face #np.zeros((img_shape[0],img.shape[1]*img.shape[2]))  #(1376, 1040)
-        #face_img = face_img.flatten()
-        #print(face_img.shape)
-
-
-        #centered = face_img - MB_matrix_mean
-        centered = face - MB_matrix_mean
-
-        #centered = centered.flatten()
-        print(centered.shape)#(1340, 1092),durch flatten (1463280,)
-        print(eigenfaces.shape) #(1463280, 130)
-
-        # and project it into the eigenface space
-        projected = np.matmul(centered, eigenfaces)
-        #print(projected.shape) #(130,)
-
-        # Now compute the similarity to all known faces
-        # (comparison is performed in the eigenface space)
-        #projected = projected.T #diese beiden operationen brauchte ich, wenn ich
-        #face_db = face_db.T    #die funktion auf nur ein bild angewand habe, jetzt mit der großen funktion scheint es überflüssig
-        #print('jetzt mit großem input face_db: \n',face_db.shape) #(1463280, 4)(130, 4)
-        #print('jetzt mit großem input projected: \n',projected.shape) #(1463280, 4)(130, 4)
-
-        distances = cdist(face_db, projected[None, :])#[None, :] das war direkt an projected
-        index = np.argmin(distances)
-        #return index
-    #    index = int(round(index /10))
-        #intex = int(index)
-        print('index: \n',index)
-        # END SOLUTION
-
-        return index
+    # def recognize_face(face, eigenfaces, mean_face, face_db):
+    #     """
+    #     Recognize a face from a face database.
+    #     and return the index of the best matching database entry.
+    #
+    #     The FACE is first centered and projected into the eigeface
+    #     space provided by EIGENFACES. Then the best match is found
+    #     according to the euclidean distance in the eigenface space.
+    #
+    #     Args:
+    #         face (ndarray): Face to be recognised.
+    #         eigenfaces (ndarray): Array of eigenfaces.
+    #         mean_face (ndarray): Average face.
+    #         face_db (ndarray): Database of faces projectected into Eigenface space.
+    #
+    #     Returns:
+    #         index (uint): Position of the best matching face in face_db.
+    #     """
+    #     index = -1
+    #
+    #     # BEGIN SOLUTION
+    #     # center the face
+    #     #face = np.reshape(img,newshape = (img_shape[0],img.shape[1]*img.shape[2]))
+    #     #face_img = face #np.zeros((img_shape[0],img.shape[1]*img.shape[2]))  #(1376, 1040)
+    #     #face_img = face_img.flatten()
+    #     #print(face_img.shape)
+    #
+    #
+    #     #centered = face_img - MB_matrix_mean
+    #     centered = face - MB_matrix_mean
+    #
+    #     #centered = centered.flatten()
+    #     print(centered.shape)#(1340, 1092),durch flatten (1463280,)
+    #     print(eigenfaces.shape) #(1463280, 130)
+    #
+    #     # and project it into the eigenface space
+    #     projected = np.matmul(centered, eigenfaces)
+    #     #print(projected.shape) #(130,)
+    #
+    #     # Now compute the similarity to all known faces
+    #     # (comparison is performed in the eigenface space)
+    #     #projected = projected.T #diese beiden operationen brauchte ich, wenn ich
+    #     #face_db = face_db.T    #die funktion auf nur ein bild angewand habe, jetzt mit der großen funktion scheint es überflüssig
+    #     #print('jetzt mit großem input face_db: \n',face_db.shape) #(1463280, 4)(130, 4)
+    #     #print('jetzt mit großem input projected: \n',projected.shape) #(1463280, 4)(130, 4)
+    #
+    #     distances = cdist(face_db, projected[None, :])#[None, :] das war direkt an projected
+    #     index = np.argmin(distances)
+    #     #return index
+    # #    index = int(round(index /10))
+    #     #intex = int(index)
+    #     print('index: \n',index)
+    #     # END SOLUTION
+    #
+    #     return index
--- code/submit_pca.py
+++ code/submit_pca.py
@@ -2,9 +2,9 @@ from grid import*
 
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/maren/asparagus/code/feature_ids.py'
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/feature_ids.py'
     path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/labeled_with_background/'
-    path_features = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/maren/asparagus/code/combined_new.csv'
+    path_features = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/combined_new.csv'
     args = [path_to_imgs, path_features]
    # env = 'source stack/bin/activate'
 
