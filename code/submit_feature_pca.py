a468df546fad9f5b3a7fcfa7aaca3ad45e811323
MalinSpaniol <mspaniol@uos.de>
2020-02-18 12:46:48 +0100

    change path to script

--- code/submit_feature_pca.py
+++ code/submit_feature_pca.py
@@ -2,7 +2,7 @@ from grid import*
 
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/feature_pca.py'
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/malin/asparagus/code/feature_pca.py'
     args = []
    # env = 'source stack/bin/activate'
 
