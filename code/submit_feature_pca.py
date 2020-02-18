ed48b82f4029eae4988c8e190721c13a0f7ad60d
MalinSpaniol <mspaniol@uos.de>
2020-02-18 12:41:35 +0100

    change submit_feature_pca.py

--- code/submit_feature_pca.py
+++ code/submit_feature_pca.py
@@ -3,7 +3,7 @@ from grid import*
 
 if __name__ == '__main__':
     path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/feature_pca.py'
-    args = [path_to_imgs, path_features]
+    args = []
    # env = 'source stack/bin/activate'
 
     submit_script(path, args)
