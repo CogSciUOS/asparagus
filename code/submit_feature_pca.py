d7a61e6990f0ac7a59c923e632f92ddd2c991231
MalinSpaniol <mspaniol@uos.de>
2020-02-19 13:30:10 +0100

    another trial

--- code/submit_feature_pca.py
+++ code/submit_feature_pca.py
@@ -4,7 +4,8 @@ from grid import*
 if __name__ == '__main__':
     path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/malin/asparagus/code/feature_pca.py'
     args = []
-    environment = 'source activate dataSet'
+    #environment = 'source activate dataSet'
+    environment = 'source net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate'
     #env = 'source stack/bin/activate'
 
     submit_script(path, args, environment)
