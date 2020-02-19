011560836328c8d91205723eb561b3a9a745ce8f
MalinSpaniol <mspaniol@uos.de>
2020-02-19 13:32:57 +0100

    minor change

--- code/submit_feature_pca.py
+++ code/submit_feature_pca.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/malin/asparagus/code/feature_pca.py'
     args = []
     #environment = 'source activate dataSet'
-    environment = 'source net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate'
+    environment = 'source /net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate dataSet'
     #env = 'source stack/bin/activate'
 
     submit_script(path, args, environment)
