bbc0c7b21f884719e2259b058a76693a08b4032b
marumse <mborn@uos.de>
2020-02-22 18:44:02 +0100

    typo

--- code/pca_processing/submit_feature_ids.py
+++ code/pca_processing/submit_feature_ids.py
@@ -4,7 +4,7 @@ from grid import*
 if __name__ == '__main__':
     path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/maren/asparagus/code/pca_processing/feature_ids.py'
     path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/labeled_with_background/'
-    path_features = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/combined_new.csv'
+    path_features = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/maren/asparagus/code/combined_new.csv'
     args = [path_to_imgs, path_features]
     env = 'source /net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate dataSet'
 
