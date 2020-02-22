09c1d03177414b9d3187b0c20c19c17c852fe92f
marumse <mborn@uos.de>
2020-02-22 12:13:29 +0100

    changed transposition projected

--- code/recognize_hollow.py
+++ code/recognize_hollow.py
@@ -40,7 +40,7 @@ def recognize(input, eigenasparagus, mean_asparagus, asparagus_space):
     print('eigenasparagus \n', eigenasparagus.shape)
 
     # and project it into the eigenface space
-    projected = np.matmul(centered, asparagus_space.T)#eigenasparagus @ centered.T
+    projected = np.matmul(centered, asparagus_space)#eigenasparagus @ centered.T
     print(projected.shape) #(400, 400)
     print(asparagus_space.shape)# (400,4)
 
