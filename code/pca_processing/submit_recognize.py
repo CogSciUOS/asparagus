5e8d81ee5de5cb7a0217effa3660054c62a66082
MalinSpaniol <mspaniol@uos.de>
2020-02-25 11:17:10 +0100

    adjust paths for submit recognize bended

--- code/pca_processing/submit_recognize.py
+++ code/pca_processing/submit_recognize.py
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     #bended
     path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/maren/asparagus/code/pca_processing/recognize_hollow.py'
     path_to_input =  '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/labeled_with_background/'# for testing (test_img)
-    path_to_PC = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_hollow/PC_bended.npy'
-    path_to_m_std = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_hollow/m_bended_std.npy'
-    path_to_space = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_hollow/m_bended_space.npy'
+    path_to_PC = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_bended/PC_bended.npy'
+    path_to_m_std = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_bended/m_bended_std.npy'
+    path_to_space = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_bended/m_bended_space.npy'
     path_to_eigenasparagus = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_bended/eig_bended_used.npy'
-    path_to_m = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_hollow/m_bended.npy'
+    path_to_m = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_bended/m_bended.npy'
     args = [path_to_input, path_to_PC, path_to_m_std, path_to_space, path_to_eigenasparagus, path_to_m]
     #environment = 'source activate dataSet'
     environment = 'source /net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate dataSet'
