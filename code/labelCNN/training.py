5ab4575087eda8667f25697c983fd9dd393ced49
k47ha <kgross@uos.de>
2020-03-02 01:26:16 +0100

    creating tf dataset

--- code/labelCNN/training.py
+++ code/labelCNN/training.py
@@ -6,6 +6,7 @@ import pandas as pd
 from logging import getLogger, StreamHandler, WARNING
 
 import tensorflow.keras as keras
+import tensorflow as tf
 
 
 log = getLogger(__file__)
@@ -49,6 +50,7 @@ def load_df(labels_csv, imagedir):
     ]
 
     # convert to float
+
     def map_label(x):
         try:
             return float(x)
@@ -93,7 +95,7 @@ def load_df(labels_csv, imagedir):
             Path(path).relative_to(Path(path).parents[2])
 
         if ready_path.is_file():
-            return ready_path
+            return str(ready_path)
         else:
             log.warning("Invalid path: %s", ready_path)
             return None
@@ -101,7 +103,7 @@ def load_df(labels_csv, imagedir):
     # process the filenames for the images
     splitted_into_3_series = df['filenames'].str.split(', ', expand=True)
 
-    # make a separate column for each of the three pictures for one asparagus piece (named: image_a, image_b, image_c)
+    # make a separate column for each of the three pichttps://pandas.pydata.org/pandas-docs/stable/user_guide/text.htmltures for one asparagus piece (named: image_a, image_b, image_c)
     for i, col in enumerate('abc'):
         df[f'image_{col}'] = splitted_into_3_series[i].transform(relative_path)
 
@@ -111,12 +113,53 @@ def load_df(labels_csv, imagedir):
     # drop rows with NaN values
     df.dropna(inplace=True)
 
-    return df
+    input_cols = [
+        'auto_width',
+        'auto_bended',
+        'auto_length',
+        'auto_violet',
+    ]
+
+    images_cols = [
+        'image_a',
+        'image_b',
+        'image_c'
+    ]
+
+    target_cols = [
+        'is_hollow',
+        'has_blume',
+        'has_rost_head',
+        'has_rost_body',
+        'is_bended',
+        'is_violet'
+    ]
+
+    for col in input_cols + target_cols:
+        df[col] = df[col].astype("float32")
+
+    for img_col in images_cols:
+        df[img_col] = df[img_col].astype("string")
+
+    return df[input_cols + images_cols], df[target_cols]
 
 
 def main(labels, imagedir):
-    df = load_df(labels, imagedir)
-    print(df.head())
+    input_df, target_df = load_df(labels, imagedir)
+    print(input_df.head())
+    print(target_df.head())
+
+    print(type(input_df))
+    print(type(target_df))
+
+    print(input_df.dtypes)
+    print(target_df.dtypes)
+
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (input_df.values, target_df.values))
+
+    for feat, targ in dataset.take(5):
+        print('Features: {}, Target: {}'.format(feat, targ))
 
 
 if __name__ == "__main__":
