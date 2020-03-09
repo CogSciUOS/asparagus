e25b81659cccd8c946cf842a0abdb44a785533db
Sophia <sschulzewedd@uos.de>
2020-03-09 15:27:29 +0100

    use my own environment

--- code/get_data/submit_model.py
+++ code/get_data/submit_model.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/stacked_no_bg_horizontal/0/data_horizontal_noB.npy'
     path_to_labels = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/get_data/combined_new.csv'
     args = [path_to_imgs, path_to_labels]
-    environment = 'source /net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate dataSet'
+    environment = 'source /net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate MultiLabel'
 
     submit_script(path, args, environment)
