50e32e335cdbde8bfc0ea9d467fe9626aae79935
unknown <sophia.schulze-weddige@gmx.de>
2020-03-12 12:27:54 +0100

    let it run on the rotated images for PCA

--- code/get_data/submit_move_files.py
+++ code/get_data/submit_move_files.py
@@ -2,9 +2,9 @@ from grid import*
 from move_files import*
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/get_data/move_files.py'
-    path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/without_background_pngs/'
-    path_to_csv = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/asparagus/code/variational_auto_encoder/combined.csv'
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sophia/asparagus/code/get_data/move_files.py'
+    path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/with_background_rotated1/'
+    path_to_csv = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sophia/asparagus/code/get_data/combined_new.csv'
     
     args = [path_to_imgs, path_to_csv]
    # env = 'source stack/bin/activate'
