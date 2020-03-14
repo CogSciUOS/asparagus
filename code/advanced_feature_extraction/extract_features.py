3f4255915ae02a215cec404de38e96865637cb34
mgerstenberg <mgerstenberg@twilight.cv.uni-osnabrueck.de>
2020-03-14 17:03:44 +0100

    ...

--- code/advanced_feature_extraction/extract_features.py
+++ code/advanced_feature_extraction/extract_features.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     input_args = parser.parse_args()
 
 
-    preprocessed_images = '/net/projects/scratch/summer/valid_until_31_January_2020/asparagus/preprocessed_images/with_background_rotated1/'
+    preprocessed_images = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/with_background_rotated1/'
     path = os.path.join(os.getcwd(),"extract_features_local.py")
     env = "source /net/projects/scratch/winter/valid_until_31_July_2020/mgerstenberg/asparagus/code/advanced_feature_extraction/feature_env/bin/activate"
     args = [preprocessed_images,os.getcwd()]
 
     if input_args.mode == "local":
         extractor = AdvancedFeatureExtractor(args[0])#preprocessed_images
-        extractor.generate_dataset(*args[1:])#outpath
+        extractor.generate_dataset1(*args[1:])#outpath
     elif input_args.mode == "grid":
         submit_script(path,args,env)
