57ea9414aead6d7912045c7b606ccc02ee4c4898
unknown <sophia.schulze-weddige@gmx.de>
2020-03-17 11:07:36 +0100

    remove class_weights

--- code/get_data/multilabel_model1.py
+++ code/get_data/multilabel_model1.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     # Train the model
     ################################################################################
     #early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)                               
-    class_weights = class_weight.compute_sample_weight(class_weight = "balanced", y = train_lbl)
+    #class_weights = class_weight.compute_sample_weight(class_weight = "balanced", y = train_lbl)
     history = model.fit(train_img, train_lbl,
                             batch_size=batch_size,
                             epochs=num_epochs,
