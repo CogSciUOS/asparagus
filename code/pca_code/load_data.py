b00a9909f69e58202e5ce5f5aba9ed425faa7a3c
MalinSpaniol <mspaniol@uos.de>
2020-03-20 13:44:03 +0100

    change load_data.py script

--- code/pca_code/load_data.py
+++ code/pca_code/load_data.py
@@ -6,16 +6,14 @@ from scipy.spatial.distance import cdist
 import os
 import sys
 import shutil
-from grid import*
-from submit_feature_pca import*
 
 
 
 eig_used = np.load('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_width/eig_width_used.py')
 
-all_pc = np.load('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_width/PC_width.py')
+#all_pc = np.load('/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_width/PC_width.py')
 
 
 print("dim aspa_space: \n",eig_used)
 
-print(all_pc)
+#print(all_pc)
