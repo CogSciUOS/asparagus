779183c6f38e0bce1d1280930ce44677feda4432
MalinSpaniol <mspaniol@uos.de>
2020-04-14 14:31:56 +0200

    create pca_calculation folder

---
+++ code/pca_code/pca_calculation/submit_feature_pca.py
@@ -0,0 +1,13 @@
+from grid import*
+
+
+if __name__ == '__main__':
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/malin/asparagus/code/pca_code/pca_processing/feature_pca.py'
+    path_to_matrix = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_hollow/m_hollow.npy'
+
+    args = [path_to_matrix]
+    #environment = 'source activate dataSet'
+    environment = 'source /net/projects/scratch/winter/valid_until_31_July_2020/asparagus/sharedConda/bin/activate dataSet'
+    #env = 'source stack/bin/activate'
+
+    submit_script(path, args, environment)
