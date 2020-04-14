eb45cfa9c28fd9796ce3405a0fdc119a3b4f3994
MalinSpaniol <mspaniol@uos.de>
2020-04-14 16:44:57 +0200

    cleaning

--- code/pca_code/pca_calculation/submit_pca_calculation.py
+++ code/pca_code/pca_calculation/submit_pca_calculation.py
@@ -2,7 +2,7 @@ from grid import*
 
 
 if __name__ == '__main__':
-    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/malin/asparagus/code/pca_code/pca_processing/feature_pca.py'
+    path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/malin/asparagus/code/pca_code/pca_processing/pca_calculation.py'
     path_to_matrix = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/data_hollow/m_hollow.npy'
 
     args = [path_to_matrix]
