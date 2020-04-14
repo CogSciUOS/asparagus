9be81dc95bf67153d0dd5ffaf6976e8ac9f4a04e
marumse <mborn@uos.de>
2020-04-14 14:27:25 +0200

    cleaned

--- code/pca_code/pca_processing/get_images/submit_feature_ids.py
+++ code/pca_code/pca_processing/get_images/submit_feature_ids.py
@@ -1,7 +1,5 @@
 from grid import*
 
-# remember here that normally path_to_imgs is labeled_with_background - I changed it to rotated for exploratory purpose
-
 if __name__ == '__main__':
     path = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/malin/asparagus/code/pca_code/pca_processing/get_ids/feature_ids.py'
     path_to_imgs = '/net/projects/scratch/winter/valid_until_31_July_2020/asparagus/preprocessed_images/labeled_with_background/'
