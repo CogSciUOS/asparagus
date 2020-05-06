491994d94acbbd8c4727f3daa1944602f3cd62e6
Richard <49528843+rich-pel@users.noreply.github.com>
2020-05-06 22:22:37 +0200

    updated file path of annotator_1.csv
    in order to work

--- tests/test_kappa.py
+++ tests/test_kappa.py
@@ -2,6 +2,6 @@ import measure_agreement
 
 def test_kappa_equals_1_if_all_same():
     # same file, so maximal agreement
-    annotations1, _annotations = measure_agreement.load_annotations("annotations/template_structure_annotation/annotator_1.csv", "annotations/template_structure_annotation/annotator_1.csv")
+    annotations1, _annotations = measure_agreement.load_annotations("labeling/annotations/template_structure_annotation/annotator_1.csv", "labeling/annotations/template_structure_annotation/annotator_1.csv")
     kappa_dict = measure_agreement.compute_agreement(annotations1,annotations1)
     assert(all(value == 1 for value in kappa_dict.values()))
